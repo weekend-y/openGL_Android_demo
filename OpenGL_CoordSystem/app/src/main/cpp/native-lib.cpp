76c01cf2b0912819db6102b4a5b3a4e056b57c8e
weekend-y <weekend45@163.com>
2022-08-04 11:27:42 +0800

    提交一个关于坐标系统的demo，实现3D效果旋转全方面查看图片

---
+++ OpenGL_CoordSystem/app/src/main/cpp/native-lib.cpp
@@ -0,0 +1,69 @@
+#include <jni.h>
+#include <string>
+#include "LogUtil.h"
+#include "MyGLRenderContext.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern "C"
+JNIEXPORT void JNICALL
+Java_com_example_opengl_1coordsystem_MyNativeRender_native_1Init(JNIEnv *env, jobject thiz) {
+    MyGLRenderContext::GetInstance();
+}
+
+extern "C"
+JNIEXPORT void JNICALL
+Java_com_example_opengl_1coordsystem_MyNativeRender_native_1UnInit(JNIEnv *env, jobject thiz) {
+    MyGLRenderContext::DestroyInstance();
+}
+
+extern "C"
+JNIEXPORT void JNICALL
+Java_com_example_opengl_1coordsystem_MyNativeRender_native_1SetImageData
+(JNIEnv *env, jobject instance, jint format, jint width, jint height, jbyteArray imageData){
+    int len = env->GetArrayLength (imageData);
+    uint8_t* buf = new uint8_t[len];
+    env->GetByteArrayRegion(imageData, 0, len, reinterpret_cast<jbyte*>(buf));
+    MyGLRenderContext::GetInstance()->SetImageData(format, width, height, buf);
+    delete[] buf;
+    env->DeleteLocalRef(imageData);
+}
+
+
+extern "C"
+JNIEXPORT void JNICALL
+Java_com_example_opengl_1coordsystem_MyNativeRender_native_1OnSurfaceCreated(JNIEnv *env,
+                                                                             jobject thiz) {
+    MyGLRenderContext::GetInstance()->OnSurfaceCreated();
+}
+
+extern "C"
+JNIEXPORT void JNICALL
+Java_com_example_opengl_1coordsystem_MyNativeRender_native_1OnSurfaceChanged(JNIEnv *env,
+                                                                             jobject thiz,
+                                                                             jint width,
+                                                                             jint height) {
+    MyGLRenderContext::GetInstance()->OnSurfaceChanged(width, height);
+}
+
+
+extern "C"
+JNIEXPORT void JNICALL
+Java_com_example_opengl_1coordsystem_MyNativeRender_native_1OnDrawFrame(JNIEnv *env, jobject thiz) {
+    MyGLRenderContext::GetInstance()->OnDrawFrame();
+}
+
+
+extern "C"
+JNIEXPORT void JNICALL
+Java_com_example_opengl_1coordsystem_MyNativeRender_native_1UpdateTransformMatrix
+(JNIEnv *env, jobject instance, jfloat rotateX, jfloat rotateY, jfloat scaleX, jfloat scaleY){
+    MyGLRenderContext::GetInstance()->UpdateTransformMatrix(rotateX, rotateY, scaleX, scaleY);
+}
+
+
+#ifdef __cplusplus
+}
+#endif
---
+++ OpenGL_CoordSystem/app/src/main/java/com/example/opengl_coordsystem/MyNativeRender.java
@@ -0,0 +1,22 @@
+package com.example.opengl_coordsystem;
+
+public class MyNativeRender {
+
+    static {
+        System.loadLibrary("native-lib");
+    }
+
+    public native void native_Init();
+
+    public native void native_UnInit();
+
+    public native void native_SetImageData(int format, int width, int height, byte[] bytes);
+
+    public native void native_UpdateTransformMatrix(float rotateX, float rotateY, float scaleX, float scaleY);
+
+    public native void native_OnSurfaceCreated();
+
+    public native void native_OnSurfaceChanged(int width, int height);
+
+    public native void native_OnDrawFrame();
+}
