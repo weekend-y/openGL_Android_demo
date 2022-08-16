67d65ad78e0e6be67ccd400af41b7626bbccd445
weekend-y <weekend45@163.com>
2022-08-16 17:48:53 +0800

    提交使用VBO/VAO/EBO等相关的demo

---
+++ OpenGL_VBOVAO/app/src/main/java/com/example/opengl_vbovao/MainActivity.java
@@ -0,0 +1,83 @@
+package com.example.opengl_vbovao;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
+
+import static android.opengl.GLSurfaceView.RENDERMODE_CONTINUOUSLY;
+
+public class MainActivity extends AppCompatActivity implements ViewTreeObserver.OnGlobalLayoutListener {
+    private static final String TAG = "MainActivity";
+    private static final String[] REQUEST_PERMISSIONS = {
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.RECORD_AUDIO,
+    };
+    private static final int PERMISSION_REQUEST_CODE = 1;
+
+    private com.example.opengl_vbovao.MyGLSurfaceView mGLSurfaceView;
+    private ViewGroup mRootView;
+    private com.example.opengl_vbovao.MyGLRender mGLRender = new com.example.opengl_vbovao.MyGLRender();
+    private SensorManager mSensorManager;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mRootView = (ViewGroup) findViewById(R.id.rootView);
+        mRootView.getViewTreeObserver().addOnGlobalLayoutListener(this);
+        mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        mGLRender.init();
+    }
+
+    @Override
+    public void onGlobalLayout() {
+        mRootView.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+        RelativeLayout.LayoutParams lp = new RelativeLayout.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+        lp.addRule(RelativeLayout.CENTER_IN_PARENT);
+        mGLSurfaceView = new com.example.opengl_vbovao.MyGLSurfaceView(this, mGLRender);
+        mRootView.addView(mGLSurfaceView, lp);
+        mGLSurfaceView.setRenderMode(RENDERMODE_CONTINUOUSLY);
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == PERMISSION_REQUEST_CODE) {
+            if (!hasPermissionsGranted(REQUEST_PERMISSIONS)) {
+                Toast.makeText(this, "We need the permission: WRITE_EXTERNAL_STORAGE", Toast.LENGTH_SHORT).show();
+            }
+        } else {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mGLRender.unInit();
+    }
+
+
+    protected boolean hasPermissionsGranted(String[] permissions) {
+        for (String permission : permissions) {
+            if (ActivityCompat.checkSelfPermission(this, permission)
+                    != PackageManager.PERMISSION_GRANTED) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+}
