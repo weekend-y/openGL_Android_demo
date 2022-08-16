3cea6e2645fc817296126f5420d7a25fafb9cbe4
weekend-y <weekend45@163.com>
2022-08-16 18:04:43 +0800

    提交OpenGL的FBO离屏渲染的demo

---
+++ OpenGL_FBO_OffscreenRender/app/src/main/cpp/FBOSample.h
@@ -0,0 +1,42 @@
+
+
+#ifndef NDK_OPENGLES_3_0_FBOSAMPLE_H
+#define NDK_OPENGLES_3_0_FBOSAMPLE_H
+
+
+#include "GLSampleBase.h"
+#include "ImageDef.h"
+
+class FBOSample : public GLSampleBase
+{
+public:
+	FBOSample();
+
+	virtual ~FBOSample();
+
+	virtual void LoadImage(NativeImage *pImage);
+
+	virtual void Init();
+	virtual void Draw(int screenW, int screenH);
+
+	virtual void Destroy();
+
+	bool CreateFrameBufferObj();
+
+private:
+	GLuint m_ImageTextureId;
+	GLuint m_FboTextureId;
+	GLuint m_FboId;
+	GLuint m_VaoIds[2];
+	GLuint m_VboIds[4];
+	GLint m_SamplerLoc;
+	NativeImage m_RenderImage;
+	GLuint m_FboProgramObj;
+	GLuint m_FboVertexShader;
+	GLuint m_FboFragmentShader;
+	GLint m_FboSamplerLoc;
+
+};
+
+
+#endif //NDK_OPENGLES_3_0_FBOSAMPLE_H
---
+++ OpenGL_FBO_OffscreenRender/app/src/main/java/com/example/opengl_fbo_offscreenrender/MainActivity.java
@@ -0,0 +1,120 @@
+package com.example.opengl_fbo_offscreenrender;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.ByteBuffer;
+
+import static android.opengl.GLSurfaceView.RENDERMODE_CONTINUOUSLY;
+import static com.example.opengl_fbo_offscreenrender.MyGLSurfaceView.IMAGE_FORMAT_RGBA;
+
+
+public class MainActivity extends AppCompatActivity implements ViewTreeObserver.OnGlobalLayoutListener {
+    private static final String TAG = "MainActivity";
+    private static final String[] REQUEST_PERMISSIONS = {
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.RECORD_AUDIO,
+    };
+    private static final int PERMISSION_REQUEST_CODE = 1;
+
+    private com.example.opengl_fbo_offscreenrender.MyGLSurfaceView mGLSurfaceView;
+    private ViewGroup mRootView;
+    private com.example.opengl_fbo_offscreenrender.MyGLRender mGLRender = new com.example.opengl_fbo_offscreenrender.MyGLRender();
+    private SensorManager mSensorManager;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mRootView = (ViewGroup) findViewById(R.id.rootView);
+        mRootView.getViewTreeObserver().addOnGlobalLayoutListener(this);
+        mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        mGLRender.init();
+        loadRGBAImage(R.drawable.dzzz);
+    }
+
+    @Override
+    public void onGlobalLayout() {
+        mRootView.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+        RelativeLayout.LayoutParams lp = new RelativeLayout.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+        lp.addRule(RelativeLayout.CENTER_IN_PARENT);
+        mGLSurfaceView = new com.example.opengl_fbo_offscreenrender.MyGLSurfaceView(this, mGLRender);
+        mRootView.addView(mGLSurfaceView, lp);
+        mGLSurfaceView.setRenderMode(RENDERMODE_CONTINUOUSLY);
+
+    }
+
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == PERMISSION_REQUEST_CODE) {
+            if (!hasPermissionsGranted(REQUEST_PERMISSIONS)) {
+                Toast.makeText(this, "We need the permission: WRITE_EXTERNAL_STORAGE", Toast.LENGTH_SHORT).show();
+            }
+        } else {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mGLRender.unInit();
+    }
+
+    private Bitmap loadRGBAImage(int resId) {
+        InputStream is = this.getResources().openRawResource(resId);
+        Bitmap bitmap;
+        try {
+            bitmap = BitmapFactory.decodeStream(is);
+            if (bitmap != null) {
+                int bytes = bitmap.getByteCount();
+                ByteBuffer buf = ByteBuffer.allocate(bytes);
+                bitmap.copyPixelsToBuffer(buf);
+                byte[] byteArray = buf.array();
+                mGLRender.setImageData(IMAGE_FORMAT_RGBA, bitmap.getWidth(), bitmap.getHeight(), byteArray);
+            }
+        }
+        finally
+        {
+            try
+            {
+                is.close();
+            }
+            catch(IOException e)
+            {
+                e.printStackTrace();
+            }
+        }
+        return bitmap;
+    }
+    protected boolean hasPermissionsGranted(String[] permissions) {
+        for (String permission : permissions) {
+            if (ActivityCompat.checkSelfPermission(this, permission)
+                    != PackageManager.PERMISSION_GRANTED) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+}
