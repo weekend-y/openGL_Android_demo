1cff84344e263e531881dab9b43562af8f94f021
weekend-y <weekend45@163.com>
2022-08-16 16:06:03 +0800

    提交openGL的混合的demo

---
+++ OpenGL_Blending/app/src/main/java/com/example/opengl_blending/MyNativeRender.java
@@ -0,0 +1,22 @@
+package com.example.opengl_blending;
+
+public class MyNativeRender {
+    public static final int SAMPLE_TYPE  =  200;
+    public static final int SAMPLE_TYPE_TEXTURE_MAP             = SAMPLE_TYPE + 1;
+
+    static {
+        System.loadLibrary("native-lib");
+    }
+
+    public native void native_Init();
+
+    public native void native_UnInit();
+
+    public native void native_OnSurfaceCreated();
+
+    public native void native_OnSurfaceChanged(int width, int height);
+
+    public native void native_SetImageDataWithIndex(int index, int format, int width, int height, byte[] bytes);
+
+    public native void native_OnDrawFrame();
+}
