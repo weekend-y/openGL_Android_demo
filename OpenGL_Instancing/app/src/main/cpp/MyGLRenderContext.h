76747a401c0db8548d0dc468b51931edff425777
weekend-y <weekend45@163.com>
2022-08-16 17:11:06 +0800

    提交openGL的实例化的demo，利用实例化绘制很多个相同但位置光照等略有不同的物体

---
+++ OpenGL_Instancing/app/src/main/cpp/MyGLRenderContext.h
@@ -0,0 +1,55 @@
+#ifndef NDK_OPENGLES_3_0_MYGLRENDERCONTEXT_H
+#define NDK_OPENGLES_3_0_MYGLRENDERCONTEXT_H
+
+#include "stdint.h"
+#include <GLES3/gl3.h>
+#include "GLSampleBase.h"
+
+#define IMAGE_FORMAT_RGBA           0x01
+#define IMAGE_FORMAT_NV21           0x02
+#define IMAGE_FORMAT_NV12           0x03
+#define IMAGE_FORMAT_I420           0x04
+#define IMAGE_FORMAT_YUYV           0x05
+#define IMAGE_FORMAT_GRAY           0x06
+#define IMAGE_FORMAT_I444           0x07
+#define IMAGE_FORMAT_P010           0x08
+
+class MyGLRenderContext
+{
+	MyGLRenderContext();
+
+	~MyGLRenderContext();
+
+public:
+	void SetImageData(int format, int width, int height, uint8_t *pData);
+
+	void SetImageDataWithIndex(int index, int format, int width, int height, uint8_t *pData);
+
+	void SetParamsInt(int paramType, int value0, int value1);
+
+	void SetParamsFloat(int paramType, float value0, float value1);
+
+	void SetParamsShortArr(short *const pShortArr, int arrSize);
+
+	void UpdateTransformMatrix(float rotateX, float rotateY, float scaleX, float scaleY);
+
+	void OnSurfaceCreated();
+
+	void OnSurfaceChanged(int width, int height);
+
+	void OnDrawFrame();
+
+	static MyGLRenderContext* GetInstance();
+	static void DestroyInstance();
+
+private:
+	static MyGLRenderContext *m_pContext;
+	GLSampleBase *m_pBeforeSample;
+	GLSampleBase *m_pCurSample;
+	int m_ScreenW;
+	int m_ScreenH;
+
+};
+
+
+#endif //NDK_OPENGLES_3_0_MYGLRENDERCONTEXT_H
