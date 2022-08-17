a9a3e4cf399a3c78f8ece5e0cd07d05959d1d50b
weekend-y <weekend45@163.com>
2022-08-17 09:53:10 +0800

    提交Transform Feedback变换反馈的一个demo

---
+++ OpenGL_TransformFeedback/app/src/main/cpp/TransformFeedbackSample.h
@@ -0,0 +1,35 @@
+
+#ifndef NDK_OPENGLES_3_0_TRANS_FEEDBACK_SAMPLE_H
+#define NDK_OPENGLES_3_0_TRANS_FEEDBACK_SAMPLE_H
+
+
+#include "GLSampleBase.h"
+#include "ImageDef.h"
+
+class TransformFeedbackSample : public GLSampleBase
+{
+public:
+	TransformFeedbackSample();
+
+	virtual ~TransformFeedbackSample();
+
+	virtual void LoadImage(NativeImage *pImage);
+
+	virtual void Init();
+	virtual void Draw(int screenW, int screenH);
+
+	virtual void Destroy();
+
+private:
+	GLuint m_ImageTextureId;
+	GLuint m_VaoId;
+	GLuint m_VboIds[2] = {GL_NONE};;
+	GLint m_SamplerLoc;
+	GLuint m_TransFeedbackObjId;
+	GLuint m_TransFeedbackBufId;
+	NativeImage m_RenderImage;
+
+};
+
+
+#endif //NDK_OPENGLES_3_0_TRANS_FEEDBACK_SAMPLE_H
