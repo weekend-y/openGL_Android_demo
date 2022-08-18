99b3584c6f44fdde4ca3355f76ed5a34e8fc2079
weekend-y <weekend45@163.com>
2022-08-18 11:14:43 +0800

    提交openGL的粒子demo，实现一个多粒子爆炸效果，利用实例化

---
+++ OpenGL_Particles/app/src/main/cpp/MyGLRenderContext.cpp
@@ -0,0 +1,124 @@
+#include "ParticlesSample.h"
+#include "MyGLRenderContext.h"
+#include "LogUtil.h"
+
+MyGLRenderContext* MyGLRenderContext::m_pContext = nullptr;
+
+MyGLRenderContext::MyGLRenderContext()
+{
+	m_pCurSample = new ParticlesSample();
+	m_pBeforeSample = nullptr;
+
+}
+
+MyGLRenderContext::~MyGLRenderContext()
+{
+	if (m_pCurSample)
+	{
+		delete m_pCurSample;
+		m_pCurSample = nullptr;
+	}
+
+	if (m_pBeforeSample)
+	{
+		delete m_pBeforeSample;
+		m_pBeforeSample = nullptr;
+	}
+
+}
+
+void MyGLRenderContext::SetImageData(int format, int width, int height, uint8_t *pData)
+{
+	LOGCATE("MyGLRenderContext::SetImageData format=%d, width=%d, height=%d, pData=%p", format, width, height, pData);
+	NativeImage nativeImage;
+	nativeImage.format = format;
+	nativeImage.width = width;
+	nativeImage.height = height;
+	nativeImage.ppPlane[0] = pData;
+
+	switch (format)
+	{
+		case IMAGE_FORMAT_NV12:
+		case IMAGE_FORMAT_NV21:
+			nativeImage.ppPlane[1] = nativeImage.ppPlane[0] + width * height;
+			break;
+		case IMAGE_FORMAT_I420:
+			nativeImage.ppPlane[1] = nativeImage.ppPlane[0] + width * height;
+			nativeImage.ppPlane[2] = nativeImage.ppPlane[1] + width * height / 4;
+			break;
+		default:
+			break;
+	}
+
+	if (m_pCurSample)
+	{
+		m_pCurSample->LoadImage(&nativeImage);
+	}
+
+}
+
+void MyGLRenderContext::OnSurfaceCreated()
+{
+	LOGCATE("MyGLRenderContext::OnSurfaceCreated");
+	glClearColor(1.0f,1.0f,1.0f, 1.0f);
+}
+
+void MyGLRenderContext::OnSurfaceChanged(int width, int height)
+{
+	LOGCATE("MyGLRenderContext::OnSurfaceChanged [w, h] = [%d, %d]", width, height);
+	glViewport(0, 0, width, height);
+	m_ScreenW = width;
+	m_ScreenH = height;
+}
+
+void MyGLRenderContext::OnDrawFrame()
+{
+	LOGCATE("MyGLRenderContext::OnDrawFrame");
+	glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
+
+	if (m_pBeforeSample)
+	{
+		m_pBeforeSample->Destroy();
+		delete m_pBeforeSample;
+		m_pBeforeSample = nullptr;
+	}
+
+	if (m_pCurSample)
+	{
+		m_pCurSample->Init();
+		m_pCurSample->Draw(m_ScreenW, m_ScreenH);
+	}
+}
+
+void MyGLRenderContext::UpdateTransformMatrix(float rotateX, float rotateY, float scaleX, float scaleY)
+{
+	LOGCATE("MyGLRenderContext::UpdateTransformMatrix [rotateX, rotateY, scaleX, scaleY] = [%f, %f, %f, %f]", rotateX, rotateY, scaleX, scaleY);
+	if (m_pCurSample)
+	{
+		m_pCurSample->UpdateTransformMatrix(rotateX, rotateY, scaleX, scaleY);
+	}
+}
+
+MyGLRenderContext *MyGLRenderContext::GetInstance()
+{
+	LOGCATE("MyGLRenderContext::GetInstance");
+	if (m_pContext == nullptr)
+	{
+		m_pContext = new MyGLRenderContext();
+	}
+	return m_pContext;
+}
+
+void MyGLRenderContext::DestroyInstance()
+{
+	LOGCATE("MyGLRenderContext::DestroyInstance");
+	if (m_pContext)
+	{
+		delete m_pContext;
+		m_pContext = nullptr;
+	}
+
+}
+
+
+
