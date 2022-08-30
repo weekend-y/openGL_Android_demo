1bcf31621547547ffacf588d93f1122f5fc3ae25
weekend-y <weekend45@163.com>
2022-08-30 11:14:31 +0800

    提交基于PBO原理实现的一个demo

---
+++ OpenGL_PBO/app/src/main/cpp/native-lib.cpp
@@ -0,0 +1,60 @@
+#include <jni.h>
+#include <string>
+#include "LogUtil.h"
+#include "MyGLRenderContext.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+extern "C"
+JNIEXPORT void JNICALL
+Java_com_example_opengl_1pbo_MyNativeRender_native_1Init(JNIEnv *env, jobject thiz) {
+    MyGLRenderContext::GetInstance();
+}
+
+extern "C"
+JNIEXPORT void JNICALL
+Java_com_example_opengl_1pbo_MyNativeRender_native_1UnInit(JNIEnv *env, jobject thiz) {
+    MyGLRenderContext::DestroyInstance();
+}
+
+extern "C"
+JNIEXPORT void JNICALL
+Java_com_example_opengl_1pbo_MyNativeRender_native_1OnSurfaceCreated(JNIEnv *env,
+                                                                          jobject thiz) {
+    MyGLRenderContext::GetInstance()->OnSurfaceCreated();
+}
+
+
+extern "C"
+JNIEXPORT void JNICALL
+Java_com_example_opengl_1pbo_MyNativeRender_native_1OnSurfaceChanged(JNIEnv *env, jobject thiz,
+                                                                          jint width, jint height) {
+    MyGLRenderContext::GetInstance()->OnSurfaceChanged(width, height);
+}
+
+extern "C"
+JNIEXPORT void JNICALL
+Java_com_example_opengl_1pbo_MyNativeRender_native_1OnDrawFrame(JNIEnv *env, jobject thiz) {
+    MyGLRenderContext::GetInstance()->OnDrawFrame();
+}
+
+
+extern "C"
+JNIEXPORT void JNICALL
+Java_com_example_opengl_1pbo_MyNativeRender_native_1SetImageData
+        (JNIEnv *env, jobject instance, jint format, jint width, jint height, jbyteArray imageData){
+    int len = env->GetArrayLength (imageData);
+    uint8_t* buf = new uint8_t[len];
+    env->GetByteArrayRegion(imageData, 0, len, reinterpret_cast<jbyte*>(buf));
+    MyGLRenderContext::GetInstance()->SetImageData(format, width, height, buf);
+    delete[] buf;
+    env->DeleteLocalRef(imageData);
+}
+
+
+#ifdef __cplusplus
+}
+#endif
---
+++ OpenGL_PBO/app/src/main/java/com/example/opengl_pbo/MyGLSurfaceView.java
@@ -0,0 +1,98 @@
+package com.example.opengl_pbo;
+
+import android.content.Context;
+import android.opengl.GLSurfaceView;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.ScaleGestureDetector;
+
+public class MyGLSurfaceView extends GLSurfaceView implements ScaleGestureDetector.OnScaleGestureListener {
+    private static final String TAG = "MyGLSurfaceView";
+
+    private final float TOUCH_SCALE_FACTOR = 180.0f / 320;
+
+    public static final int IMAGE_FORMAT_RGBA = 0x01;
+    public static final int IMAGE_FORMAT_NV21 = 0x02;
+    public static final int IMAGE_FORMAT_NV12 = 0x03;
+    public static final int IMAGE_FORMAT_I420 = 0x04;
+    public static final int IMAGE_FORMAT_YUYV = 0x05;
+    public static final int IMAGE_FORMAT_GARY = 0x06;
+
+    private MyGLRender mGLRender;
+
+    private int mRatioWidth = 0;
+    private int mRatioHeight = 0;
+
+    private ScaleGestureDetector mScaleGestureDetector;
+    private float mPreScale = 1.0f;
+    private float mCurScale = 1.0f;
+    private long mLastMultiTouchTime;
+
+
+    public MyGLSurfaceView(Context context, MyGLRender glRender) {
+        this(context, glRender, null);
+    }
+
+    public MyGLSurfaceView(Context context, MyGLRender glRender, AttributeSet attrs) {
+        super(context, attrs);
+        this.setEGLContextClientVersion(2);
+        mGLRender = glRender;
+        /*If no setEGLConfigChooser method is called,
+        then by default the view will choose an RGB_888 surface with a depth buffer depth of at least 16 bits.*/
+        setEGLConfigChooser(8, 8, 8, 8, 16, 8);
+        setRenderer(mGLRender);
+        setRenderMode(RENDERMODE_WHEN_DIRTY);
+        mScaleGestureDetector = new ScaleGestureDetector(context, this);
+
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        int width = MeasureSpec.getSize(widthMeasureSpec);
+        int height = MeasureSpec.getSize(heightMeasureSpec);
+
+        if (0 == mRatioWidth || 0 == mRatioHeight) {
+            setMeasuredDimension(width, height);
+        } else {
+            if (width < height * mRatioWidth / mRatioHeight) {
+                setMeasuredDimension(width, width * mRatioHeight / mRatioWidth);
+            } else {
+                setMeasuredDimension(height * mRatioWidth / mRatioHeight, height);
+            }
+        }
+    }
+
+    public void setAspectRatio(int width, int height) {
+        Log.d(TAG, "setAspectRatio() called with: width = [" + width + "], height = [" + height + "]");
+        if (width < 0 || height < 0) {
+            throw new IllegalArgumentException("Size cannot be negative.");
+        }
+
+        mRatioWidth = width;
+        mRatioHeight = height;
+        requestLayout();
+    }
+
+    public MyGLRender getGLRender() {
+        return mGLRender;
+    }
+
+    @Override
+    public boolean onScale(ScaleGestureDetector detector) {
+        return false;
+    }
+
+    @Override
+    public boolean onScaleBegin(ScaleGestureDetector detector) {
+        return true;
+    }
+
+    @Override
+    public void onScaleEnd(ScaleGestureDetector detector) {
+        mPreScale = mCurScale;
+        mLastMultiTouchTime = System.currentTimeMillis();
+
+    }
+
+}
