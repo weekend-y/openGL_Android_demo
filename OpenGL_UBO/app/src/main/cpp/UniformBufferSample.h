c3fedc0525613f2a3b9081d6ef7deaf2a633a641
weekend-y <weekend45@163.com>
2022-09-02 16:25:03 +0800

    提交基于UBO的一个渲染demo

---
+++ OpenGL_UBO/app/src/main/cpp/UniformBufferSample.h
@@ -0,0 +1,47 @@
+#ifndef NDK_OPENGLES_3_0_UNIFORMBUFFERSAMPLE_H
+#define NDK_OPENGLES_3_0_UNIFORMBUFFERSAMPLE_H
+
+#include <detail/type_mat.hpp>
+#include <detail/type_mat4x4.hpp>
+#include "GLSampleBase.h"
+
+class UniformBufferSample : public GLSampleBase
+{
+public:
+	UniformBufferSample();
+
+	virtual ~UniformBufferSample();
+
+	virtual void LoadImage(NativeImage *pImage);
+
+	virtual void Init();
+	virtual void Draw(int screenW, int screenH);
+
+	virtual void Destroy();
+
+	virtual void UpdateTransformMatrix(float rotateX, float rotateY, float scaleX, float scaleY);
+
+	void UpdateMVPMatrix(glm::mat4 &mvpMatrix, int angleX, int angleY, float ratio);
+
+private:
+	GLuint m_TextureId;
+	GLint m_SamplerLoc;
+	GLint m_MVPMatLoc;
+	GLuint m_VaoId;
+	GLuint m_UboId;
+	GLuint m_VboIds[3];
+	NativeImage m_RenderImage;
+	glm::mat4 m_MVPMatrix;
+    glm::mat4 m_ModelMatrix;
+    glm::mat4 m_ViewMatrix;
+    glm::mat4 m_ProjectionMatrix;
+
+	int m_AngleX;
+	int m_AngleY;
+	float m_ScaleX;
+	float m_ScaleY;
+
+};
+
+
+#endif //NDK_OPENGLES_3_0_UNIFORMBUFFERSAMPLE_H
---
+++ OpenGL_UBO/app/src/main/java/com/example/opengl_ubo/MyGLRender.java
@@ -0,0 +1,48 @@
+package com.example.opengl_ubo;
+
+import android.opengl.GLSurfaceView;
+import android.util.Log;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+public class MyGLRender implements GLSurfaceView.Renderer {
+    private static final String TAG = "MyGLRender";
+    private MyNativeRender mNativeRender;
+    private int mSampleType;
+
+    MyGLRender() {
+        mNativeRender = new MyNativeRender();
+    }
+
+    @Override
+    public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+        mNativeRender.native_OnSurfaceCreated();
+        Log.e(TAG, "onSurfaceCreated() called with: GL_VERSION = [" + gl.glGetString(GL10.GL_VERSION) + "]");
+    }
+
+    @Override
+    public void onSurfaceChanged(GL10 gl, int width, int height) {
+        mNativeRender.native_OnSurfaceChanged(width, height);
+
+    }
+
+    @Override
+    public void onDrawFrame(GL10 gl) {
+        mNativeRender.native_OnDrawFrame();
+
+    }
+
+    public void setImageData(int format, int width, int height, byte[] bytes) {
+        mNativeRender.native_SetImageData(format, width, height, bytes);
+    }
+
+    public void init() {
+        mNativeRender.native_Init();
+    }
+
+    public void unInit() {
+        mNativeRender.native_UnInit();
+    }
+
+}
