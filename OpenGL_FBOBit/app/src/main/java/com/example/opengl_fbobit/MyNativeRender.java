c26642bb5bf82d864b48005dbac551acdd5c9050
weekend-y <weekend45@163.com>
2022-09-02 11:40:13 +0800

    提交基于openGL ES3.0的帧缓冲区位块传送技术编写的一个demo

---
+++ OpenGL_FBOBit/app/src/main/java/com/example/opengl_fbobit/MyNativeRender.java
@@ -0,0 +1,22 @@
+package com.example.opengl_fbobit;
+
+public class MyNativeRender {
+    public static final int SAMPLE_TYPE  =  200;
+    public static final int SAMPLE_TYPE_TEXTURE_MAP             = SAMPLE_TYPE + 1;
+
+    static {
+        System.loadLibrary("native-lib");
+    }
+
+    public native void native_Init();
+
+    public native void native_UnInit();
+
+    public native void native_SetImageData(int format, int width, int height, byte[] bytes);
+
+    public native void native_OnSurfaceCreated();
+
+    public native void native_OnSurfaceChanged(int width, int height);
+
+    public native void native_OnDrawFrame();
+}
